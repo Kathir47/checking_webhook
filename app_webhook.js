82bd7ba8af91043dc855c836828d84d835cf8c65
Kathir47 <kathirthiru7@gmail.com>
2026-04-03 06:01:58 +0000

    changed a line in app_webhooks

--- app_webhook.js
+++ app_webhook.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello from inside Docker!</h1><p>Port mapping is working!</p>');
+    res.end('<h1>Hello from inside Docker!</h1><p>Port mapping is not working!!!!</p>');
 });
 
 server.listen(3333, () => {
